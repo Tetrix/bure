b1be0dc9168738af1ce268934c1b0d27f1dd7424
shakle17 <saklevaleksandar@gmail.com>
2015-08-25 20:51:26 +0200

    new penguin

--- game/character.py
+++ game/character.py
@@ -1,14 +1,18 @@
 import pygame
 
+
 main_char = "resources/penguin.png"
 
 class Character(object):
     def __init__(self):
-        self.character = pygame.image.load(main_char).convert_alpha()
-        self.character=pygame.transform.scale(self.character,(35,55))
-        self.rect = self.character.get_rect()
+        
+        self.character = pygame.image.load(main_char).convert()
+        self.character.set_colorkey((0,0,0))
+        self.character =pygame.transform.scale(self.character,(35,55))
+        self.character_rect = self.character.get_rect()
+        self.character_mask = pygame.mask.from_surface(self.character)
     
     def properties(self):
-        self.char_x = 100
-        self.char_y = 300
-        self.char_speed = 200
+        self.char_x = 337
+        self.char_y = 482
+        self.char_speed = 300
---
+++ game/wheel.py
@@ -0,0 +1,21 @@
+import pygame
+from gameobjects.vector2 import Vector2
+
+wheel_file= "resources/circle.png"
+
+class Wheel(object):
+    def __init__(self):
+        self.wheel = pygame.sprite.Sprite()
+        self.wheel=pygame.image.load(wheel_file).convert_alpha()
+        
+        
+    def wheel_properties(self):
+        
+        self.wheel_pos = Vector2(450, 300)
+        self.wheel_speed = 300.
+        self.wheel_ue_speed=500
+        self.wheel_rotation = 0.
+        self.wheel_rotation_speed = -50. # Degrees per second
+        self.wheel_rotate=pygame.USEREVENT + 1
+        self.wheel_rotation_direction = 0.
+        pygame.time.set_timer(self.wheel_rotate,self.wheel_ue_speed)
