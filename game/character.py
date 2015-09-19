7779b8b65a4f5fd757b4325e4b1ef9e8d178d301
Tetrix <deniporjazovski@yahoo.com>
2015-09-19 13:51:00 +0200

    Timer added

--- game/character.py
+++ game/character.py
@@ -1,7 +1,9 @@
 import pygame
-
-
+window_x = 270
+window_y = 80
 main_char = "resources/penguin.png"
+import os
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (window_x,window_y)
 
 class Character(object):
     def __init__(self):
@@ -10,16 +12,27 @@ class Character(object):
         self.main_char = "resources/penguin.png"
         self.character = pygame.image.load(self.main_char).convert()
         self.character.set_colorkey((0,0,0))
-        self.character = pygame.transform.scale(self.character,(45,45))
+        self.character = pygame.transform.scale(self.character,(35,35))
         self.character_rect = self.character.get_rect()
         self.character_mask = pygame.mask.from_surface(self.character)
-    
-    def properties(self):
         self.char_x = 100
         self.char_y = 100
         self.char_speed = 170
+    
+    
 
         
-     
+    def char_out_of_screen(self):
+        if self.char_x >= 765:
+            self.char_x = 765
+        if self.char_x <= 0:
+            self.char_x = 0
+        if self.char_y <= 0:
+            self.char_y = 0
+        if self.char_y >= 545:
+            self.char_y = 545      
     
        
+       
+
+    
---
+++ game/check_sum.py
@@ -0,0 +1,46 @@
+import time
+import pygame
+from pygame.locals import USEREVENT
+from gameobjects.vector2 import Vector2
+
+pygame.init()
+
+lvl_finish_f = 'resources/bugara.png'
+game_over_f= 'resources/game-over.png'
+go_coordinates=Vector2(200,150)
+sumfont = pygame.font.Font("resources/fonts/goall.otf", 60)
+screen = pygame.display.set_mode((800,600),0,32)
+lvl_finish = pygame.image.load(lvl_finish_f).convert_alpha()
+lvl_finish = pygame.transform.scale(lvl_finish,(600,300))
+game_over = pygame.image.load(game_over_f).convert_alpha()
+
+
+def check_sum(numbers_chosen,zbir,suma,level,same_level):
+    
+    #import game
+    for num in numbers_chosen:
+        suma += num
+    if zbir == suma:
+        print "UDZU"
+        screen.blit(lvl_finish,(142,100))
+        pygame.time.set_timer(USEREVENT+6,20)
+        for event in pygame.event.get():
+            if event.type == USEREVENT+6:
+               time.sleep(1)
+               level()
+ 
+    if zbir < suma:
+       screen.blit(game_over,go_coordinates)
+       pygame.time.set_timer(USEREVENT+7,20)
+       for event in pygame.event.get():
+          if event.type == USEREVENT+7:
+            time.sleep(1)
+            same_level()   
+    
+    
+
+def check_the_sum(numbers_chosen,zbir,suma,color):
+    for num in numbers_chosen:
+        suma += num
+    summ = sumfont.render(str(suma),1,color)
+    return summ
